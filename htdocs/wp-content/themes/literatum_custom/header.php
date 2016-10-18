af2d8ec7cbdcd713879c16a5617bdf9cd349d353
AJ Magnuson <albert.magnuson@gmail.com>
2016-10-17 21:23:56 -0700

    header retro rainbow 2

--- htdocs/wp-content/themes/literatum_custom/header.php
+++ htdocs/wp-content/themes/literatum_custom/header.php
@@ -18,6 +18,7 @@
 
 <body <?php body_class(); ?>>
 <div style="float:left;height:5px;width:20%;margin:0;background-color:#f60" class="bg_adrien orange"></div><div style="float:left;height:5px;width:20%;margin:0;background-color:#f7efcb" class="bg_adrien orange"></div><div style="float:left;height:5px;width:20%;margin:0;background-color:#afd5bd" class="bg_adrien orange"></div><div style="float:left;height:5px;width:20%;margin:0;background-color:#56a49f" class="bg_adrien orange"></div><div style="float:left;height:5px;width:20%;margin:0;background-color:#1e1b10" class="bg_adrien orange">
+<div style="clear:both"></div>
 
 <!--[if IE 9 ]>
 <div id="page" class="hfeed site ie9">
