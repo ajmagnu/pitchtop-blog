3d1d4e962c41d3d0220871e0b53a0b475b943ea3
AJ Magnuson <albert.magnuson@gmail.com>
2016-10-17 21:25:05 -0700

    header retro rainbow 4

--- htdocs/wp-content/themes/literatum_custom/header.php
+++ htdocs/wp-content/themes/literatum_custom/header.php
@@ -26,8 +26,7 @@
 <div id="page" class="hfeed site">
 <!--<![endif]-->
 	
-	<div style="float:left;height:5px;width:20%;margin:0;background-color:#f60" class="bg_adrien orange"></div><div style="float:left;height:5px;width:20%;margin:0;background-color:#f7efcb" class="bg_adrien orange"></div><div style="float:left;height:5px;width:20%;margin:0;background-color:#afd5bd" class="bg_adrien orange"></div><div style="float:left;height:5px;width:20%;margin:0;background-color:#56a49f" class="bg_adrien orange"></div><div style="float:left;height:5px;width:20%;margin:0;background-color:#1e1b10" class="bg_adrien orange">
-<div style="clear:both"></div>
+	
 
 
 
@@ -39,7 +38,8 @@
 
     <!-- Push Wrapper -->
     <div class="mp-pusher " id="mp-pusher">
-
+<div style="float:left;height:5px;width:20%;margin:0;background-color:#f60" class="bg_adrien orange"></div><div style="float:left;height:5px;width:20%;margin:0;background-color:#f7efcb" class="bg_adrien orange"></div><div style="float:left;height:5px;width:20%;margin:0;background-color:#afd5bd" class="bg_adrien orange"></div><div style="float:left;height:5px;width:20%;margin:0;background-color:#56a49f" class="bg_adrien orange"></div><div style="float:left;height:5px;width:20%;margin:0;background-color:#1e1b10" class="bg_adrien orange">
+<div style="clear:both"></div>
 
 
 
